c94aef1095783cfa44390349ffe438e508b0bfc1
Darryl Kilzer <darrylkilzer@gmail.com>
2017-04-20 16:12:29 -0600

    syntax fix

--- app.js
+++ app.js
@@ -1,25 +1,98 @@
-let counter = 0;
 
-function petCat() {
-  counter++;
-  document.getElementById("petCount").innerText = counter;
-  if (counter > 5) {
-    document.getElementById("catStatus").innerText = "RAWR! You got scratched.";
+let cats = [{
+  id: 1,
+  name: "Fluffy",
+  imgUrl: "http://thecatapi.com/api/images/get?format=src&type=gif",
+  happyStatus: "Purrrrrrrr",
+  grumpyStatus: "Raaaawr",
+  feralStatus: "HIISSSSS",
+  goneStatus: "Mr. Fluffy ran away!",
+  petCount: 0
+},
+{
+  id: 2,
+  name: "Whiskers",
+  imgUrl: "https://unsplash.it/200/200",
+  happyStatus: "meeeeow",
+  grumpyStatus: "MROWL",
+  feralStatus: "HSSSSAAASRWL",
+  goneStatus: "Whiskers has run away!",
+  petCount: 0
+}];
+
+function petCat(catId) {
+
+  let petCount = document.getElementById("petCount" + catId);
+  let catStatus = document.getElementById("catStatus" + catId);
+  let catButton = document.getElementById("pet" + catId);
+  let catPic = document.getElementById("catPic" + catId);
+  let targetCat = getCatById(catId);
+
+  targetCat.petCount++;
+  petCount.innerHTML = targetCat.petCount;
+  if (targetCat.petCount > 5) {
+    catStatus.innerHTML = targetCat.grumpyStatus;
   }
-  if (counter > 9) {
-    document.getElementById("catStatus").innerText = "HISSSSS";
+  if (targetCat.petCount > 9) {
+    catStatus.innerHTML = targetCat.feralStatus;
   }
-  if (counter > 10) {
-    document.getElementById("catStatus").innerText = "The cat has had ENUFF!(he ran)";
-    document.getElementById("pet").disabled = true;
-    document.getElementById("catPic").src="http://www.barebooks.com/wp-content/uploads/2013/10/GM05.gif"
+  if (targetCat.petCount > 10) {
+    catStatus.innerHTML = targetCat.petCount;
+    catButton.disabled = true;
+    catPic.src = "";
   }
 }
 
-function treatCat() {
-  counter = 0;
-  document.getElementById("petCount").innerText = counter;
-  document.getElementById("pet").disabled = false;
-  document.getElementById("catStatus").innerText = "The cat seems Happy, for now....";
-  document.getElementById("catPic").src="http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg"
+function getCatById(catId) {
+  for (var i = 0; i < cats.length; i++) {
+
+    let cat = cats[i];
+
+    if (cat.id == catId) {
+      return cat;
+    }
+  }
 }
+function treatCat(catId) {
+  let petCount = document.getElementById("petCount" + catId);
+  let catStatus = document.getElementById("catStatus" + catId);
+  let catButton = document.getElementById("pet" + catId);
+  let catPic = document.getElementById("catPic" + catId);
+  let targetCat = getCatById(catId);
+
+  targetCat.petCount = 0;
+  petCount.innerHTML = targetCat.petCount;
+  catButton.disabled = false;
+  catStatus.innerHTML = targetCat.happyStatus;
+  catPic.src = targetCat.imgUrl;
+}
+
+function drawCats(listOfCats) {
+
+  let template = "";
+
+  for (let i = 0; i < listOfCats.length; i++) {
+    var cat = listOfCats[i];
+    template += `
+  <div>
+    <img id="catPic${cat.id}" src="${cat.imgUrl}" alt="">
+    <button
+      type="button"
+      id="pet${cat.id}"
+      onclick="petCat(${cat.id})">
+      Pet Me</button>
+    <button
+      type="button"
+      onclick="treatCat(${cat.id})">
+      Use a Treat</button>
+    <h1
+      id="petCount${cat.id}">0</h1>
+    <h2
+      id="catStatus${cat.id}">${cat.happyStatus}</h2>
+  </div>
+  `
+  }
+  document.getElementById("cat-list").innerHTML = template
+}
+
+drawCats(cats)
